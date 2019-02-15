481f5dc6a3abf5b0fdb14d8e1b93365fd7e63066
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-02-15 17:18:37 +0300

    heading put in a text box

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
-function getValues() {
-     var length A = parseInt(document.getElementById('length A').value);
-
-     function getValues() {
+ function getValues() {
        var lengthA = parseInt(document.getElementById('length A').value);
        var lengthB = parseInt(document.getElementById('length B').value);
        var lengthC = parseInt(document.getElementById('length C').value);
        var sides = [length A, length B, length C]
 
-       if ((length A + length B) <= length C || (length A + length C) <= length B || (length B + length C) <= length A) {
-         alert("This is not a triangle.");
-       } else if ((length A === length B) && (length A === length C) && (length B === length C)) {
-         alert("The values you gave, " + sides[0] + " " + sides[1] + " " + sides[2] + ", make up an equilateral triangle.");
-       } else if ((length A === length C) && (length B != length A) || (length A === length B) && (length C != length A) || (length B === length C) && (length 2 != length A)) {
-         alert("The values you gave, " + sides[0] + " " + sides[1] + " " + sides[2] + ", make up an isoceles triangle.");
-       } else {
-         alert("The values you gave, " + sides[0] + " " + sides[1] + " " + sides[2] + ", make up a scalene triangle.");
-       }
-     }
+       if ((lengthA ===lengthB) && (lengthB===lengthC)) {
+     alert("This is an Equilateral Triangle");
+   } else if ((lengthA > (lengthB + lengthC)) || (lengthB > (lengthA + lengthC)) || (lengthC > (lengthA + lengthB))) {
+     alert("This is an NOT a Triangle");
+   } else if ((lengthA === lengthB) || (lengthA === lengthC) || (lengthB === lengthC)) {
+     alert("This is an Isosceles Triangle");
+   } else if ((lengthA !== lengthB !== lengthC) && (lengthA + lengthB > lengthC) || (lengthB + lengthC > lengthA) || (lengthA + lengthC > lengthB)) {
+   alert("This is an Isosceles Triangle");
+ } else {
+   alert("Please enter new trinagle lengths");
+ }
+
+}
