3f2113fb4ab7fa5c3e8be8ce21c347883e862420
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-02-15 17:59:37 +0300

    javascript functions added

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
  function getValues() {
-       var lengthA = parseInt(document.getElementById('length A').value);
-       var lengthB = parseInt(document.getElementById('length B').value);
-       var lengthC = parseInt(document.getElementById('length C').value);
-       var sides = [length A, length B, length C]
+   var lengthA = parseInt(document.getElementById('lengthA').value);
+   var lengthB = parseInt(document.getElementById('lengthB').value);
+   var lengthC = parseInt(document.getElementById('lengthC').value);
+   var sides = [lengthA, lengthB, lengthC]
 
-       if ((lengthA ===lengthB) && (lengthB===lengthC)) {
-     alert("This is an Equilateral Triangle");
-   } else if ((lengthA > (lengthB + lengthC)) || (lengthB > (lengthA + lengthC)) || (lengthC > (lengthA + lengthB))) {
+
+   if ((lengthA > (lengthB + lengthC)) || (lengthB > (lengthA + lengthC)) || (lengthC > (lengthA + lengthB))) {
      alert("This is an NOT a Triangle");
+   } else if ((lengthA === lengthB) && (lengthB === lengthC)) {
+     alert("This is an Equilateral Triangle");
    } else if ((lengthA === lengthB) || (lengthA === lengthC) || (lengthB === lengthC)) {
      alert("This is an Isosceles Triangle");
    } else if ((lengthA !== lengthB !== lengthC) && (lengthA + lengthB > lengthC) || (lengthB + lengthC > lengthA) || (lengthA + lengthC > lengthB)) {
-   alert("This is an Isosceles Triangle");
- } else {
-   alert("Please enter new trinagle lengths");
- }
+     alert("This is an Isosceles Triangle");
+   } else {
+     alert("Please enter new trinagle lengths");
+   }
 
-}
+ }
